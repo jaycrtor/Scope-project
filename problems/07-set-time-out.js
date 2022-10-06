895ea1cfa90ca82264516e5eacb3c7ea70ed28ac
lingxiGitHub <lingxi2015fall@gmail.com>
2022-10-06 14:13:32 -0700

    finish07

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -19,6 +19,18 @@ funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 ***********************************************************************/
 
 // Your code here
+function funcTimer(time, func){
+ setTimeout(func,time)
+}
+
+
+
+
+// function restFunc() {
+//   console.log("rest time!")
+// }
+
+// funcTimer(5000, restFunc); // in 5 seconds prints: "Party time!"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
