bdb2a200e88382958964253fb292b352621f8788
lingxiGitHub <lingxi2015fall@gmail.com>
2022-10-06 13:03:37 -0700

    finish 03

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -12,6 +12,18 @@ arrowRestSum(0); // => 0
 
 // Your code here
 
+let arrowRestSum = (...num) => {
+
+let sum=0;
+  for(let i=0; i<num.length; i++) {
+    sum =sum+num[i]
+  }
+  return sum
+}
+arrowRestSum(3, 5, 6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+arrowRestSum(0); // => 0
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
