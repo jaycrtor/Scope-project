d5b695beb227fc2ef5f480eda859a2030d399840
lingxiGitHub <lingxi2015fall@gmail.com>
2022-10-06 15:31:26 -0700

    10 is so hard

--- problems/10-count-down.js
+++ problems/10-count-down.js
@@ -29,6 +29,44 @@ Example 4:
 ***********************************************************************/
 
 // Your code here
+// function countDownTimer(n){
+//   let word = "Happy New Year!"
+//   for (let i=n; i>=0; i--) {
+// if (i===0){
+//   return word
+// } else {
+//   return function(){}
+// }
+// }
+// }
+
+function countDownTimer(n) {
+
+if (n===0){
+  return "Happy New Year!"
+} 
+
+const count=()=>{
+
+  n-=1
+  if (n===0) {
+    return "Happy New Year!"
+  } else {
+    return count;
+  }
+}
+return count;
+}
+
+
+
+//console.log(countDownTimer(0)); // prints "Happy New Year!"
+
+let threeDays = countDownTimer(3); // returns a function
+console.log(threeDays()); // returns a function
+console.log(threeDays()); // returns a function
+console.log(threeDays()); // prints "Happy New Year!"
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
