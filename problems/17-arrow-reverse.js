af5fdf870947ebd0af83783b003a4f7f2ce42164
Jay <nhocchuot1995@gmail.com>
2022-10-09 12:08:33 -0400

    prob 17

--- problems/17-arrow-reverse.js
+++ problems/17-arrow-reverse.js
@@ -5,14 +5,20 @@ reversed.
 Write this function using a fat arrow function!
 
 Examples:
+
+
+
+***********************************************************************/
+const arrowReverseString = str => {
+let split = (str.split(''))
+let reverse = split.reverse()
+return reverse.join('')
+
+}
 let result1 = arrowReverseString("hello"); // returns "olleh"
 let result2 = arrowReverseString("garden"); // returns "nedrag"
 let result3 = arrowReverseString("potato"); // returns "otatop"
 
-
-***********************************************************************/
-
-// Your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 
--- problems/18-planned-intersection.js
+++ problems/18-planned-intersection.js
@@ -14,9 +14,22 @@ let fame = plannedIntersect(["f", "a", "m", "e"]); // returns a function
 console.log(fame(["a", "f", "z", "b"])); // returns [ 'f', 'a' ]
 
 *******************************************************************************/
+function plannedIntersect(firstArr){
+  let arr = [];
+  return function(secondArr){
 
+
+
+    return arr
+  }
+}
 // Your code here
+let abc = plannedIntersect(["a", "b", "c"]); // returns a function
+console.log(abc(["b", "d", "c"])); // returns [ 'b', 'c' ]
 
+// Example 2:
+let fame = plannedIntersect(["f", "a", "m", "e"]); // returns a function
+console.log(fame(["a", "f", "z", "b"])); // returns [ 'f', 'a' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 
 try {
